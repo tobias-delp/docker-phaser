79be5d80b9f6d2243dabeb770154b3e29161047d
Chris Langton <chris.langton@cmdsolutions.com.au>
2018-03-31 20:34:28 +1100

    added photonstorm/phaser3-project-template and dockerhub helpers

---
+++ push-versions.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+WORKDIR=`pwd`
+declare -a versions=(
+  "3.0" "3.0.0" 
+  "3.1" "3.1.0" "3.1.1" "3.1.2"
+  "3.2" "3.2.0" "3.2.1"
+  "3.3" "3.3.0"
+)
+
+source phaser.bash
+source .env
+phaser-stop
+
+if [ -z ${DOCKERHUB_USER} ] || [ -z ${PROJECT_NAME} ]; then
+  echo "variables [DOCKERHUB_USER,PROJECT_NAME] not fond"
+  exit 1
+fi
+if [ -z ${PHASER_PORT} ]; then
+  PHASER_PORT=3000
+fi
+if [ -z ${PHASER_INDEX} ]; then
+  PHASER_INDEX=src/index.html
+fi
+
+LAST_VERSION=''
+for VERSION in "${versions[@]}"
+do
+  if [ -z ${LAST_VERSION} ]; then
+    for v in "${versions[@]}"
+    do
+      docker rmi ${DOCKERHUB_USER}/${PROJECT_NAME}:${v}
+    done
+  else
+    docker rmi ${DOCKERHUB_USER}/${PROJECT_NAME}:${LAST_VERSION}
+  fi
+  cd ${WORKDIR}/docker/${VERSION}/
+  docker build . \
+  --build-arg PHASER_PORT=${SERVER_PORT} \
+  --build-arg PHASER_INDEX=${PHASER_INDEX} \
+  --compress \
+  --force-rm \
+  --rm \
+  -f ${WORKDIR}/docker/${VERSION}/Dockerfile \
+  -t ${DOCKERHUB_USER}/${PROJECT_NAME}:${VERSION}
+
+  dockerhub-push ${VERSION}
+  LAST_VERSION=$VERSION
+done
+
+cd $WORkDIR
+docker rmi ${DOCKERHUB_USER}/${PROJECT_NAME}:${LAST_VERSION}
