ec7fa2afa4bd44a7dd0ba508f70f070c75dcc11a
Christopher Langton <chrislangton84@gmail.com>
2018-07-18 13:46:50 +1000

    merger phaser version bump (#3)
    * version bump
    
    * Create .travis.yml
    
    * travisci
    
    * travisci
    
    * travisci
    
    * travisci needs to be more granular
    
    * travisci needs to be more granular
    
    * travisci needs to be more granular
    
    * travisci log limits
    
    * travisci ltime limits
    
    * adding version 3.9

--- build-versions.sh
+++ build-versions.sh
@@ -14,31 +14,16 @@ declare -a versions=(
   "3.9" "3.9.0"
 )
 
-source phaser.bash
-source .env
-phaser-stop
-
-if [ -z ${DOCKERHUB_USER} ] || [ -z ${PROJECT_NAME} ]; then
-  echo "variables [DOCKERHUB_USER,PROJECT_NAME] not fond"
-  exit 1
-fi
-if [ -z ${PHASER_PORT} ]; then
-  PHASER_PORT=3000
-fi
-if [ -z ${PHASER_INDEX} ]; then
-  PHASER_INDEX=src/index.html
-fi
-
 LAST_VERSION=''
 for VERSION in "${versions[@]}"
 do
   if [ -z ${LAST_VERSION} ]; then
     for v in "${versions[@]}"
     do
-      docker rmi ${DOCKERHUB_USER}/${PROJECT_NAME}:${v} 2>/dev/null
+      docker rmi ci/docker-phaser:${v} 2>/dev/null
     done
   else
-    docker rmi ${DOCKERHUB_USER}/${PROJECT_NAME}:${LAST_VERSION}
+    docker rmi ci/docker-phaser:${LAST_VERSION}
   fi
   cd ${WORKDIR}/docker/${VERSION}/
   docker build . \
@@ -46,12 +31,10 @@ do
   --force-rm \
   --rm \
   -f ${WORKDIR}/docker/${VERSION}/Dockerfile \
-  -t ${DOCKERHUB_USER}/${PROJECT_NAME}:${VERSION}
+  -t ci/docker-phaser:${VERSION}
 
-  cd $WORKDIR
-  dockerhub-push ${VERSION}
   LAST_VERSION=$VERSION
 
 done
 
-docker rmi ${DOCKERHUB_USER}/${PROJECT_NAME}:${LAST_VERSION}
+docker rmi ci/docker-phaser:${LAST_VERSION}
